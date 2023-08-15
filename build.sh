3328934dd5a2a2e04b44cdb985ef1e99492ad1b4
ChiefGorilla <67667960+ChiefGorilla@users.noreply.github.com>
2023-08-15 18:29:17 +1000

    updated

---
+++ build.sh
@@ -0,0 +1,11 @@
+# Install Flask
+pip3 install flask
+
+# Install OpenAI
+pip3 install openai
+
+# Run migrations (if applicable)
+python manage.py migrate
+
+# Compile static resources (if applicable)
+python manage.py collectstatic --noinput
